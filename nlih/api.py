f60727d9ce19c0c7547fa9c5884c8f8a87096b78
abdullah <abdukul18@gmail.com>
2024-07-22 08:09:22 +0100

    removed controller

--- nlih/api.py
+++ nlih/api.py
@@ -3,7 +3,6 @@ from app.api import router as api_router
 from app.controllers.authentication.register import router as signup_handler
 from app.controllers.authentication.signin import router as signin_handler
 from app.controllers.appointments.appointments import router as appointment_router
-from ninja_jwt.controller import NinjaJWTDefaultController
 
 api = NinjaAPI()
 
@@ -12,4 +11,3 @@ api.add_router("/v1/", signup_handler)
 api.add_router("/v1/", signin_handler)
 api.add_router("/v1/", appointment_router)
 
-api.register_controllers(NinjaJWTDefaultController)
